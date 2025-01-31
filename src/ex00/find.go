3adbdbbe0505eca4930538f4cdfb17aeb80fc6d4
amisyurov <amisyurov.c@arlo.com>
2025-01-31 22:19:54 +0500

    temp version

--- src/ex00/find.go
+++ src/ex00/find.go
@@ -7,6 +7,64 @@ import (
 	"path/filepath"
 )
 
+const (
+	FIND_FILES 		= 1 << 0
+	FIND_DIRS 		= 1 << 1
+	FIND_SYMLINKS 	= 1 << 2
+)
+
+type Finder interface {
+	Print(in_path string)
+}
+
+type DirFinder struct {}
+type FileFinder struct {}
+type SymLinkFinder struct {}
+
+func(dir DirFinder) Print(in_path string) {
+	entries, err := os.ReadDir(in_path)
+	if err != nil {
+		return
+	}
+	for _, entry := range entries {
+		full_path := filepath.Join(in_path, entry.Name())
+		if entry.IsDir() {
+			fmt.Println(full_path + "/")
+			dir.Print(full_path)
+		}
+	}
+}
+
+func(file FileFinder) Print(in_path string) {
+	entries, err := os.ReadDir(in_path)
+	if err != nil {
+		return
+	}
+	for _, entry := range entries {
+		full_path := filepath.Join(in_path, entry.Name())
+		if entry.IsDir() {
+			file.Print(full_path)
+		} else if entry.Type().IsRegular() {
+			fmt.Println(full_path)
+		}
+	}
+}
+
+func(sl SymLinkFinder) Print(in_path string) {
+	entries, err := os.ReadDir(in_path)
+	if err != nil {
+		return
+	}
+	for _, entry := range entries {
+		full_path := filepath.Join(in_path, entry.Name())
+		if entry.IsDir() {
+			sl.Print(full_path)
+		} else if entry.Type() & os.ModeSymlink != 0 {
+			fmt.Println(full_path)
+		}
+	}
+}
+
 func print_data(in_path string) {
 	entries, err := os.ReadDir(in_path)
 	if err != nil {
@@ -24,39 +82,40 @@ func print_data(in_path string) {
 }
 
 func main() {
+
+	var mask int
+	_ = mask
+
 	f_file := flag.Bool("f", false, "file name to search")
 	f_dir := flag.Bool("d", false, "directory name to search")
 	f_symlink := flag.Bool("sl", false, "symlink name to search")
 	// f_ext := flag.String("ext", "", "extantion files to search. works only if -f is specified")
 	flag.Parse()
 
-	count := 0
-	if *f_file {
-		count += 1
-	}
-	if *f_dir {
-		count += 1
-	}
-	if *f_symlink {
-		count += 1
-	}
-
-	if count > 1 {
-		fmt.Println("err: you should call only one flag")
-		os.Exit(1)
-	}
-
+	var finder Finder
+	
 	args := flag.Args()
 	if len(args) < 0 {
 		fmt.Println("err: empty path to directory")
 		os.Exit(1)
 	}
 
-	print_data(args[0])
+	if *f_file {
+		mask = mast & FIND_FILES
+		finder = FileFinder{}
+		finder.Print(args[0])
+	}
+	if *f_dir {
+		mask = mast & FIND_DIRS
+		finder = DirFinder{}
+		finder.Print(args[0])
+	}
+	if *f_symlink {
+		mask = mast & FIND_SYMLINKS
+		finder = SymLinkFinder{}
+		finder.Print(args[0])
+	}
 
-	// fmt.Println(*f_file)
-	// fmt.Println(*f_dir)
-	// fmt.Println(*f_symlink)
-	// fmt.Println(path)
+
 
 }
