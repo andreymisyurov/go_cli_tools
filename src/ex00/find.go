4328368f0f323e51a6eb22a15731a4708d0de39a
amisyurov <amisyurov.c@arlo.com>
2025-02-03 21:23:07 +0500

    Finish find func

--- src/ex00/find.go
+++ src/ex00/find.go
@@ -1,27 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 const (
-	FIND_FILES 		= 1 << 0
-	FIND_DIRS 		= 1 << 1
-	FIND_SYMLINKS 	= 1 << 2
+	FIND_FILES    = 1 << 0
+	FIND_DIRS     = 1 << 1
+	FIND_SYMLINKS = 1 << 2
+	FIND_EXT      = 1 << 3
 )
 
-type Finder interface {
-	Print(in_path string)
-}
-
-type DirFinder struct {}
-type FileFinder struct {}
-type SymLinkFinder struct {}
-
-func(dir DirFinder) Print(in_path string) {
+func Finder(in_path string, mask uint8, in_ext string) {
+	in_path, _ = filepath.Abs(in_path)
 	entries, err := os.ReadDir(in_path)
 	if err != nil {
 		return
@@ -29,93 +24,58 @@ func(dir DirFinder) Print(in_path string) {
 	for _, entry := range entries {
 		full_path := filepath.Join(in_path, entry.Name())
 		if entry.IsDir() {
-			fmt.Println(full_path + "/")
-			dir.Print(full_path)
-		}
-	}
-}
-
-func(file FileFinder) Print(in_path string) {
-	entries, err := os.ReadDir(in_path)
-	if err != nil {
-		return
-	}
-	for _, entry := range entries {
-		full_path := filepath.Join(in_path, entry.Name())
-		if entry.IsDir() {
-			file.Print(full_path)
-		} else if entry.Type().IsRegular() {
-			fmt.Println(full_path)
-		}
-	}
-}
-
-func(sl SymLinkFinder) Print(in_path string) {
-	entries, err := os.ReadDir(in_path)
-	if err != nil {
-		return
-	}
-	for _, entry := range entries {
-		full_path := filepath.Join(in_path, entry.Name())
-		if entry.IsDir() {
-			sl.Print(full_path)
-		} else if entry.Type() & os.ModeSymlink != 0 {
-			fmt.Println(full_path)
-		}
-	}
-}
-
-func print_data(in_path string) {
-	entries, err := os.ReadDir(in_path)
-	if err != nil {
-		return
-	}
-	for _, entry := range entries {
-		full_path := filepath.Join(in_path, entry.Name())
-		if entry.IsDir() {
-			fmt.Println(full_path + "/")
-			print_data(full_path)
-		} else {
-			fmt.Println(full_path)
+			if mask&FIND_DIRS != 0 {
+				fmt.Println(full_path + "/")
+			}
+			Finder(full_path, mask, in_ext)
+		} else if entry.Type().IsRegular() && mask&FIND_FILES != 0 {
+			if mask&FIND_EXT == 0 || strings.HasSuffix(full_path, in_ext) {
+				fmt.Println(full_path)
+			}
+		} else if entry.Type()&os.ModeSymlink != 0 && mask&FIND_SYMLINKS != 0 {
+			_, err := filepath.EvalSymlinks(full_path)
+			if err != nil {
+				fmt.Println(full_path, "-> [broken]")
+			} else {
+				fmt.Println(full_path, "->", full_path)
+			}
 		}
 	}
 }
 
 func main() {
 
-	var mask int
-	_ = mask
+	var mask uint8
 
 	f_file := flag.Bool("f", false, "file name to search")
 	f_dir := flag.Bool("d", false, "directory name to search")
 	f_symlink := flag.Bool("sl", false, "symlink name to search")
-	// f_ext := flag.String("ext", "", "extantion files to search. works only if -f is specified")
+	f_ext := flag.String("ext", "", "extantion files to search. works only if -f is specified")
 	flag.Parse()
-
-	var finder Finder
-	
 	args := flag.Args()
-	if len(args) < 0 {
+	if len(args) == 0 {
 		fmt.Println("err: empty path to directory")
 		os.Exit(1)
 	}
 
 	if *f_file {
-		mask = mast & FIND_FILES
-		finder = FileFinder{}
-		finder.Print(args[0])
+		mask |= FIND_FILES
+		if *f_ext != "" {
+			mask |= FIND_EXT
+		}
 	}
 	if *f_dir {
-		mask = mast & FIND_DIRS
-		finder = DirFinder{}
-		finder.Print(args[0])
+		mask |= FIND_DIRS
 	}
 	if *f_symlink {
-		mask = mast & FIND_SYMLINKS
-		finder = SymLinkFinder{}
-		finder.Print(args[0])
+		mask |= FIND_SYMLINKS
+	}
+	if mask == 0 {
+		mask = FIND_DIRS | FIND_FILES | FIND_SYMLINKS
 	}
 
-
+	for _, arg := range args {
+		Finder(arg, mask, *f_ext)
+	}
 
 }
