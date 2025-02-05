5f179e170f3e463cef9048e81b04c4c830cfc6ef
amisyurov <amisyurov.c@arlo.com>
2025-02-05 21:47:42 +0500

    refactoring

--- src/ex03/rotate.go
+++ src/ex03/rotate.go
@@ -15,6 +15,7 @@ import (
 
 func create_gz(in_path string, wg *sync.WaitGroup, target_path string) {
 	defer wg.Done()
+
 	file, err := os.OpenFile(in_path, os.O_RDONLY, 0)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error opening file %s: %v\n", in_path, err)
@@ -25,32 +26,42 @@ func create_gz(in_path string, wg *sync.WaitGroup, target_path string) {
 	info, err := file.Stat()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error getting file info %s: %v\n", in_path, err)
+		return
 	}
+
 	mtime := info.ModTime().Unix()
-	archive_path := fmt.Sprintf("%s_%d.tar.gz", strings.TrimSuffix(filepath.Base(in_path), filepath.Ext(in_path)), mtime)
+	archive_path := fmt.Sprintf("%s_%d.tar.gz",
+		strings.TrimSuffix(filepath.Base(in_path), filepath.Ext(in_path)), mtime)
+
 	if target_path != "" {
 		archive_path = path.Join(target_path, archive_path)
 	}
+
+	if err := write_tar(file, archive_path, info); err != nil {
+		fmt.Fprintf(os.Stderr, "Error archiving file %s: %v\n", in_path, err)
+	}
+}
+
+func write_tar(file *os.File, archive_path string, info os.FileInfo) error {
 	out_file, err := os.Create(archive_path)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error creating archive %s: %v\n", archive_path, err)
-		return
+		return fmt.Errorf("error creating archive %s: %w", archive_path, err)
 	}
 	defer out_file.Close()
 
-	gz_writter := gzip.NewWriter(out_file)
-	defer gz_writter.Close()
-	tar_writer := tar.NewWriter(gz_writter)
+	gz_writer := gzip.NewWriter(out_file)
+	defer gz_writer.Close()
+	tar_writer := tar.NewWriter(gz_writer)
 	defer tar_writer.Close()
+
 	header := &tar.Header{
-		Name: filepath.Base(in_path),
+		Name: filepath.Base(file.Name()),
 		Size: info.Size(),
 		Mode: int64(info.Mode()),
 	}
 
 	if err := tar_writer.WriteHeader(header); err != nil {
-		fmt.Fprintf(os.Stderr, "Error writing tar header: %v\n", err)
-		return
+		return fmt.Errorf("error writing tar header: %w", err)
 	}
 
 	buf := make([]byte, 4096)
@@ -59,15 +70,15 @@ func create_gz(in_path string, wg *sync.WaitGroup, target_path string) {
 		if err == io.EOF {
 			break
 		} else if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading file %s: %v\n", in_path, err)
-			return
+			return fmt.Errorf("error reading file %s: %w", file.Name(), err)
 		}
 		if _, err := tar_writer.Write(buf[:n]); err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing to tar: %v\n", err)
-			return
+			return fmt.Errorf("error writing to tar: %w", err)
 		}
 	}
-	fmt.Printf("Archived: %s -> %s\n", in_path, archive_path)
+
+	fmt.Printf("Archived: %s -> %s\n", file.Name(), archive_path)
+	return nil
 }
 
 func main() {
